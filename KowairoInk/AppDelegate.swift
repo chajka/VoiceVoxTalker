7dacf4ef26f976206612ef7a69ef0823b5f29ecb
Чайка <chajka.niconico@gmail.com>
2023-12-12 11:01:12 +0900

    Initial Commit

---
+++ KowairoInk/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  KowairoInk
+//
+//  Created by Чайка on 2023/12/12.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+		return true
+	}
+
+
+}
+
